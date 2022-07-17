8b782942f33eae126692fa1331d75352a6e2ffe5
dangell7 <denisangell7@gmail.com>
2022-07-17 03:50:41 -0400

    types

---
+++ Tests/XRPLSwiftTests/Unit/BinaryCodec/Types/TestCurrency.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Denis Angell on 7/11/22.
+//
+
+import Foundation
---
+++ Tests/XRPLSwiftTests/Unit/BinaryCodec/Types/TestPathSet.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Denis Angell on 7/16/22.
+//
+
+import Foundation
